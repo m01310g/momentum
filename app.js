b8bc05a4f9e9adaaf8603283c4d351c9df2bdbe4
m01310g <m01310g@gmail.com>
2024-08-25 23:49:55 +0900

    using variables

--- app.js
+++ app.js
@@ -4,3 +4,16 @@ console.log("lalalalal");
 console.log(5+2);
 console.log(5/2);
 console.log(5*2);
+
+// variable 사용
+const a = 5;
+const b = 2;
+
+// js는 camelCase 주로 사용
+const myName = "m01310g";
+
+console.log(a+b);
+console.log(a/b);
+console.log(a*b);
+
+console.log("hello "+myName);
