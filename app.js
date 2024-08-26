4d144201ea2d338d7a4677790bcac65a6f6a97f9
m01310g <m01310g@gmail.com>
2024-08-26 16:46:48 +0900

    array

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-// boolean 사용
-// null은 존재하지 않음, 자연적으로 발생하지 않고 선언을 해줘야 함(값이 존재하지 않음을 의도적으로 표현)
-// undefined는 변수가 메모리에는 있는데 값이 존재하지 않음(비어 있음)
-// false는 '거짓'이라는 값이 존재
-const amIFat = null;
-let something;  // something -> undefined
+// array
+const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"];
 
-console.log(something, amIFat);
+console.log(daysOfWeek);
+
+// 인덱싱
+console.log(daysOfWeek[4]);
+
+// 배열에 아이템 추가
+daysOfWeek.push("sun");
+
+console.log(daysOfWeek);
