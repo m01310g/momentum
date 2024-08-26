21e6f29c73be6ba11bdd1dbf531ea88c7c5f78ef
m01310g <m01310g@gmail.com>
2024-08-26 16:35:00 +0900

    booleans, undefined, null

--- app.js
+++ app.js
@@ -1,22 +1,8 @@
-// variable 사용
-// const -> 값 변경 불가
-const a = 5;
-const b = 2;
+// boolean 사용
+// null은 존재하지 않음, 자연적으로 발생하지 않고 선언을 해줘야 함(값이 존재하지 않음을 의도적으로 표현)
+// undefined는 변수가 메모리에는 있는데 값이 존재하지 않음(비어 있음)
+// false는 '거짓'이라는 값이 존재
+const amIFat = null;
+let something;  // something -> undefined
 
-// js는 camelCase 주로 사용
-const myName = "m01310g";
-
-console.log(a+b);
-console.log(a/b);
-console.log(a*b);
-
-console.log("hello "+myName);
-
-// let 사용해서 variable 정의
-// let -> 값 변경 가능
-let c = 5;
-let d = 2;
-
-console.log(c+d);
-console.log(c/d);
-console.log(c*d);
+console.log(something, amIFat);
