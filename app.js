1fd5723a038e1f9e467a9128c8b842cf007d3ef0
m01310g <m01310g@gmail.com>
2024-08-26 17:00:34 +0900

    using objects

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
-// array
-const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"];
+// const playerName = "m01310g";
+// const playerPoints = 121212;
+// const playerHandsome = true;
+// const playerFat = "little bit";
 
-console.log(daysOfWeek);
+// array -> 어떤 특성인지 알려주지 못하고 있음
+// const player = ["m01310g", 121212, true, "little bit"];
 
-// 인덱싱
-console.log(daysOfWeek[4]);
+// object 이용
 
-// 배열에 아이템 추가
-daysOfWeek.push("sun");
+const player = {
+    name: "m01310g",
+    points: 10,
+    fat: false,
+};
 
-console.log(daysOfWeek);
+console.log(player);
+console.log(player.name);
+
+player.fat = true;
+console.log(player);
+
+// 특성 추가
+player.lastName = "Kim";
+console.log(player);
