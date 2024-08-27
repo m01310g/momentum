2117f5293175efd99155e8028f69c03afec4370d
m01310g <m01310g@gmail.com>
2024-08-27 16:07:43 +0900

    Conditionals

--- app.js
+++ app.js
@@ -1,40 +1,25 @@
-// 계산기
-// function에 return 사용
+// Conditionals(조건문)
 
-const calculator = {
-    // 더하기
-    add: function(a, b) {
-        return a + b;
-    },
-    // 빼기
-    minus: function(a, b) {
-        return a - b;
-    },
-    // 나누기
-    divide: function(a, b) {
-        return a / b;
-    },
-    // 곱하기
-    multi: function(a, b) {
-        return a * b;
-    },
-    // 제곱
-    power: function(a, b) {
-        return a ** b ;
-    }
-};
+// prompt -> 사용자에게 창을 띄우고 값을 입력하도록 설정, 하지만 오래된 방법이라 잘 사용되지 않음
+// parseInt -> string을 number로 변환해줌
 
-const plusResult = calculator.add(1, 2);
-console.log(plusResult);
+const age = parseInt(prompt("How old are you?"));
 
-const minusResult = calculator.minus(plusResult, 3);
-console.log(minusResult);
+// console.log(age);
 
-const timesResult = calculator.multi(10, minusResult);
-console.log(timesResult);
+// isNaN -> Boolean 값을 반환함 number이면 false, 아니면 true 반환
 
-const divideResult = calculator.divide(timesResult, plusResult);
-console.log(divideResult);
-
-const powerResult = calculator.power(divideResult, minusResult);
-console.log(powerResult);
+// ||(OR) 연산자 -> 둘 중 하나의 조건만 참이면 참
+if (isNaN(age) || age < 0) {
+    // isNaN(age) == true일 경우, number가 아니므로 다시 입력 받아야 함
+    console.log("Please write a real positive number.");
+} else if (age < 18) {
+    console.log("You are too young.");
+    // &&(AND) 연산자 -> 모든 조건이 단 만족해야 참
+} else if (age >= 18 && age <= 50) { 
+    console.log("You can drink.")
+} else if (age > 50 && age <= 80) {
+    console.log("You should exercise.");
+} else if (age > 80) {
+    console.log("You can do whatever you want.");
+}
