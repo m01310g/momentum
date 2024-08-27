69a652f2833a73ab1b255666a33597ba24cc166b
m01310g <m01310g@gmail.com>
2024-08-27 14:52:24 +0900

    calculator

--- app.js
+++ app.js
@@ -1,32 +1,30 @@
-// functions
+// 계산기
 
-// function sayHello(nameOfPerson, age) {
-//     console.log("Hello my name is " + nameOfPerson + " and I'm " + age);
-// }
-
-// sayHello("Capybara", 10);
-// sayHello("Gyodon", 12);
-
-// 함수의 인수 a, b는 함수 내부에서만 존재
-// 함수 외부에서 console.log(a) 하면 a가 존재하지 않기 때문에 오류 발생
-function plus(a, b) {
-    console.log(a + b);
-}
-
-plus(1, 4);
-
-function divide(a, b) {
-    console.log(a / b);
-}
-
-divide(98, 20);
-
-const player = {
-    name: "capybara",
-    sayHello: function(otherPersonsName) {
-        console.log("hello " + otherPersonsName + " nice to meet you");
+const calculator = {
+    // 더하기
+    add: function(a, b) {
+        console.log(a + b);
     },
+    // 빼기
+    minus: function(a, b) {
+        console.log(a - b);
+    },
+    // 나누기
+    divide: function(a, b) {
+        console.log(a / b);
+    },
+    // 곱하기
+    multi: function(a, b) {
+        console.log(a * b);
+    },
+    // 제곱
+    power: function(a, b) {
+        console.log(a ** b);
+    }
 };
 
-console.log(player.name);
-player.sayHello("gyodon");
+calculator.add(1, 2);
+calculator.minus(4, 3);
+calculator.divide(10, 2);
+calculator.multi(20, 3);
+calculator.power(3, 4);
