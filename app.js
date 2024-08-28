1494005b7ba32de79798ac70391c40c20d2dbb65
m01310g <m01310g@gmail.com>
2024-08-28 17:59:24 +0900

    js on browser with events

--- app.js
+++ app.js
@@ -1,25 +1,58 @@
-// Conditionals(조건문)
+// js는 html을 읽어옴
+// console에 document 입력 -> 작성한 html 불러오기 가능
+// js를 통해 html을 수정 또한 가능
 
-// prompt -> 사용자에게 창을 띄우고 값을 입력하도록 설정, 하지만 오래된 방법이라 잘 사용되지 않음
-// parseInt -> string을 number로 변환해줌
+// getElementById -> id를 이용해 element를 가져옴, className이나 innerText를 더 자주 사용
+// tag name을 이용해서 관련 element의 array를 모두 불러올 수 있음
+// const title = document.getElementsByTagName("h1");
 
-const age = parseInt(prompt("How old are you?"));
 
-// console.log(age);
+// CSS Selector 형태로 원하는 element를 불러옴, 한 개의 element만 return
+const h1 = document.querySelector(".Hello h1"); 
+console.log(h1);
 
-// isNaN -> Boolean 값을 반환함 number이면 false, 아니면 true 반환
-
-// ||(OR) 연산자 -> 둘 중 하나의 조건만 참이면 참
-if (isNaN(age) || age < 0) {
-    // isNaN(age) == true일 경우, number가 아니므로 다시 입력 받아야 함
-    console.log("Please write a real positive number.");
-} else if (age < 18) {
-    console.log("You are too young.");
-    // &&(AND) 연산자 -> 모든 조건이 단 만족해야 참
-} else if (age >= 18 && age <= 50) { 
-    console.log("You can drink.")
-} else if (age > 50 && age <= 80) {
-    console.log("You should exercise.");
-} else if (age > 80) {
-    console.log("You can do whatever you want.");
+function handleTitleClick() {
+    h1.style.color = "blue";
+    console.log("title was clicked!");
 }
+
+function handleMouseEnter() {
+    h1.innerText = "Mouse is here!";
+}
+
+function handleMouseLeave() {
+    h1.innerText = "Mouse is gone!";
+}
+
+function handleWindowResize() {
+    document.body.style.backgroundColor = "tomato";
+}
+
+function handleWindowCopy() {
+    alert("copier!");
+}
+
+function handleWindowOffline() {
+    alert("SOS no WIFI");
+}
+
+function handleWindowOnline() {
+    alert("All Good");
+}
+
+// click event 발생하는지 확인, 함수 넣을 때 괄호는 필요 없음
+// h1.addEventListener("click", handleTitleClick);
+h1.onclick = handleTitleClick;
+
+// 조건에 부합하는 모든 element return
+// const title2 = document.querySelectorAll(".Hello h1");
+// console.log(title2);
+
+// event를 찾는 방법 -> e.g. h1 html element mdn 검색, Wbe APIs 페이지 확인, HTMLelement
+h1.addEventListener("mouseenter", handleMouseEnter);
+h1.addEventListener("mouseleave", handleMouseLeave);
+
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offline", handleWindowOffline);
+window.addEventListener("online", handleWindowOnline);
