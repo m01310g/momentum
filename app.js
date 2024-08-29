71fc6020eff946b90928b21efae8ee31270b4085
m01310g <m01310g@gmail.com>
2024-08-29 23:42:35 +0900

    saving & loading username(localStorage)

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");   // greeting id를 불러옴
 
-const link = document.querySelector("a");
+const HIDDEN_CLASSNAME = "hidden"
+const USERNAME_KEY = "username"
 
 // 함수가 argument를 받도록 하고, 그걸 JS에 넘겨줌
 // argument는 공간을 만들어준 것
@@ -9,21 +11,29 @@ function onLoginSubmit(event) {
     // preventDefault 함수는 어떤 event의 기본 행동이 발생되지 않도록 막는 역할
     event.preventDefault();
     const username = loginInput.value;
-    console.log(username);
+
+    // localStorage에 username 저장(Key-Value 쌍)
+    localStorage.setItem(USERNAME_KEY, username);
+
+    paintGreetings(username);
 }
 
-function handleLinkClick(event) {
-    // preventDefault 때문에 기본 동작 실행 x -> 링크로 이동되지 않음
-    event.preventDefault();
-    console.log(event);
-    // alert가 페이지가 다른 동작을 못하도록 막음, OK 클릭해서 alert 사라지면 기본 동작 실행
-    // alert("clicked!");
+function paintGreetings(username) {
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
-
-// loginForm의 submit event 감지
-// event가 발생할 때 브라우저가 function 호출
 
 // EventListener function의 첫번째 argument는 항상 방금 막 일어난 일에 대한 정보
 loginForm.addEventListener("submit", onLoginSubmit);    // form을 submit 할 때 입력값 받아냄(페이지가 자동으로 새로고침)
 
-link.addEventListener("click", handleLinkClick);
+// username 값이 있으면 form 대신 greeting 보여주기
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+    // show the form
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    // show the greeting
+    paintGreetings(savedUsername);
+}
